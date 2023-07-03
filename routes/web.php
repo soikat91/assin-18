4853fc53bf5c70e4b9f84c113dfebe2b9a583da8
soikat <soikateman@gmail.com>
2023-07-03 14:40:31 +0600

    final work done

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,6 @@ use App\Http\Controllers\DemoController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// probelm-2
 
 //task-5
 Route::get('/getPost',[PostController::class,'getPost']);
